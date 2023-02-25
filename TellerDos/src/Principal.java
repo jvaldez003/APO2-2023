67c111ae00314e49fa573e7eaab3b569cddccce7
jvaldez003 <125087380+jvaldez003@users.noreply.github.com>
2023-02-24 23:58:36 -0500

    Add files via upload

---
+++ TellerDos/src/Principal.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Principal {
+	public static void main(String[] args) {
+		AreaDeTriangulo tr= new AreaDeTriangulo();
+		tr.calculararea();
+		
+		Autobus au= new Autobus();
+		au.registroautobus();
+		
+		
+		
+		
+		
+		
+		
+	}
+}
